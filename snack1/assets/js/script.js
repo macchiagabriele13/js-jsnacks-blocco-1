ffde9cf61d7c51a5a1bf3abb6b3d7144fc8c5355
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-12 11:46:23 +0200

    snack 1 - terminato

---
+++ snack1/assets/js/script.js
@@ -0,0 +1,21 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+/* Prompt richiesta */
+const firstNumberEl = prompt("Inserisci un numero")
+console.log(firstNumberEl);
+
+
+const secondNumberEl = prompt("Inserisci un nuovo numero")
+console.log(secondNumberEl);
+
+
+let winner;
+if (firstNumberEl > secondNumberEl) {
+    winner = firstNumberEl
+} else {
+    winner = secondNumberEl
+}
+
+
+document.getElementById("winner").innerHTML = ("Il numero vincente è " + `${winner}`);
