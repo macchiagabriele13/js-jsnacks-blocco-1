e1e82abb1c0dffaecc55ec4053e3945cbdb8fe90
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-12 12:18:23 +0200

    snack 2 pre correzione

---
+++ snack2/js/script.js
@@ -0,0 +1,25 @@
+/* /* L’utente inserisce due parole in successione, con due prompt. */
+
+const firstWordEl = prompt("Inserisci una parola")
+console.log(firstWordEl);
+
+const secondWordEl = prompt("Inserisci una nuova parola")
+console.log(secondWordEl);
+
+let lettere = firstWordEl.split('') + secondWordEl.split('')
+
+
+let max = lettere[0];
+
+
+for(var i = 0; i < lettere.length; i++){
+  if(lettere[i].length > max.length ){
+    max = lettere[i];
+  }
+}
+
+console.log('la parola più lunga è: ' + max);
+
+
+
+/* Il software stampa prima la parola più corta, poi la parola più lunga. */
