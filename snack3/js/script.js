23afa143f0d62f289954472aa5e6e0fde148e848
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-12 12:44:53 +0200

    snack 3 - solo prompt

---
+++ snack3/js/script.js
@@ -0,0 +1,14 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero. */
+
+
+
+for (let i = 0; i < 10; i++) {
+    const number = Number(prompt("Inserisci un numero"))
+    console.log(number);
+   
+
+    /* const singleNumber = number[i] + [i];
+    console.log(singleNumber); */
+    
+}
+/* Il programma stampa la somma di tutti i numeri inseriti.  */
