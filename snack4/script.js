600c24d89260ea1f264c14c45c08f0734f25326c
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-13 12:47:56 +0200

    snack 4 - non corretto

---
+++ snack4/script.js
@@ -0,0 +1,46 @@
+/* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, */
+
+const listName = [
+    'Marco',
+    'Mary',
+    'Phil',
+    'June',
+]
+/* 
+chiedi all’utente il suo nome */
+const inviteName = prompt('Qual è il tuo nome?')
+console.log(inviteName);
+
+/* Verifica se il nome è nella lista */
+
+let message = false;
+let i = 0
+while (i < listName.length) {
+    const validName = listName[i]
+    console.log(validName);
+
+    if (inviteName == validName) {
+        message = true ;
+        console.log(message, 'Benveuto');
+    }
+  i++
+
+}
+
+let userMessage = document.querySelector("h1");
+  
+if (message = true) {
+
+    userMessage.innerHTML = "Benvenuto" 
+    
+} else {
+userMessage.innerHTML = "Ci spiace, non è invitato alla festa"
+}
+
+
+
+
+
+
+
+ /* comunicagli se può partecipare o no alla festa. */
